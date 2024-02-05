bc6a756105dece2ca2af95da0a3c6339a1101d7d
Emanuel Padilla <emanuehl159@gmail.com>
2024-02-05 16:55:39 -0500

    Update tab formatting, hide notifications and change theme to catppuccin

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,12 +1,16 @@
 return {
-  {
-    "navarasu/onedark.nvim",
-    opts = { style = "cool" },
-  },
+  -- {
+  --   "navarasu/onedark.nvim",
+  --   opts = { style = "cool" },
+  -- },
+  -- {
+  --   "olimorris/onedarkpro.nvim",
+  --   -- priority = 1000, -- Ensure it loads first
+  -- },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onedark",
+      colorscheme = "catppuccin",
     },
   },
 }

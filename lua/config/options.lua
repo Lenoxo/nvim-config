7ea2ab290997acc08bdb6ebc7a343c058b57858f
Emanuel Padilla <emanuehl159@gmail.com>
2024-05-25 14:07:01 -0500

    Add dap and prettier support, and remove unnecesary conform config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,10 @@ vim.o.tabstop = 2 -- A TAB character looks like 2 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
+
+-- nvim-cmp capabilities
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = false
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.preselectSupport = true
+capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
